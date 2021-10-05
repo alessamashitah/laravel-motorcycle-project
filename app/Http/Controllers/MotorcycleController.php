6a94ea71f94f94f6ee2e4636cecc62fd5261f733
alessamashitah <alessa.mashitahzj@gmail.com>
2021-10-05 15:27:10 +0800

    Controller updated

---
+++ app/Http/Controllers/MotorcycleController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Motorcycle;
+use Illuminate\Http\Request;
+
+class MotorcycleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $motorcycle = Motorcycle::all();
+
+        return view('motorcycle.index',compact('motorcycle'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('motorcycle.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //STORE GUNA RELATIONSHIP
+        $motorcycle = new Motorcycle();
+        $motorcycle->type = $request->type;
+        $motorcycle->colour = $request->colour;
+        $motorcycle->save();
+
+        return redirect()->route('motorcycleindex')->with([
+            'alert-type' => 'alert-primary',
+            'alert'=> 'New motorcycle has been added'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(motorcycle $motorcycle)
+    {
+        return view('motorcycle.edit',compact('motorcycle'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, motorcycle $motorcycle)
+    {
+        $motorcycle->type = $request->type;
+        $motorcycle->colour = $request->colour;
+        $motorcycle->save();
+
+        return redirect()->route('motorcycleindex')->with([
+            'alert-type' => 'alert-primary',
+            'alert'=> 'New motorcycle has been updated'
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function delete(motorcycle $motorcycle)
+    {
+        $motorcycle->delete();
+
+        return redirect()->route('motorcycleindex')->with([
+            'alert-type' => 'alert-primary',
+            'alert'=> 'New motorcycle has been deleted'
+        ]);
+    }
+}
