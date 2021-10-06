59f131fc46bc5b0ee54a8af71d943677415b6e42
alessamashitah <alessa.mashitahzj@gmail.com>
2021-10-06 14:29:03 +0800

    add function for motorType and motorColour

--- app/Models/Motorcycle.php
+++ app/Models/Motorcycle.php
@@ -11,12 +11,22 @@ class Motorcycle extends Model
 
     protected $fillable = [
         'id',
-        'type',
-        'colour',
+        'motor_type_id',
+        'motor_colour_id',
     ];
 
     public function Users()
     {
         return $this->hasMany(User::class);
+    }
+
+    public function motorType()
+    {
+        return $this->belongsTo(Motor_type::class, 'motor_type_id');
+    }
+
+    public function motorColour()
+    {
+        return $this->belongsTo(Motor_colour::class, 'motor_colour_id');
     }
 }
